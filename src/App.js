23f69de2af2e0444972c6576c593e26f71904f3b
Shifan1225 <dengshifan@gmail.com>
2023-01-03 00:24:04 -0800

    Update App.js
    Update app.js to todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
 
 function App() {
+  const [todos, setTodos] = useState([
+    { name: 'Read a book', done: true },
+    { name: 'Write a blog', done: true },
+    { name: 'Make a video', done: false },
+    { name: 'Reply a comment', done: true },
+  ]);
+  const [todo, setTodo] = useState('');
+  const handleClick = () => {
+    if (!todo) {
+      return;
+    }
+    const newTodos = todos.concat([{ name: todo, done: false }]);
+    setTodos(newTodos);
+    setTodo('');
+  }
+  const handleChange = (e) => {
+    const todo = e.target.value;
+    setTodo(todo);
+    console.log({todo});
+  }
+
+  const toggleDone = (e, index) => {
+    const newTodos = todos.map((todo, i) => {
+      if (i !== index) {
+        return todo;
+      }
+      return { name: todo.name, done: !todo.done };
+    });
+    setTodos(newTodos);
+  }
+
+  const TotalRemaining = todos.filter((todo) => {
+    return todo.done === false;
+  }).length;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todo List</h1>
+      <div>TotalRemaining: {TotalRemaining}</div>
+      <div>
+        <input type="text" onChange={handleChange} value={todo} />
+        <button onClick={handleClick}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, i) => {
+          return ( 
+            <li>
+              <input 
+                type="checkbox"
+                onClick={(e) => toggleDone(e, i)}
+                checked={todo.done ? 'checked' : ''}
+              ></input>
+              {todo.name}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
